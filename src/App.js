e15ad0d6049fa96675c03cd5f02a924624bd154c
Rejaul Islam <rejaraju2k19@gmail.com>
2022-12-10 16:40:35 +0600

    login-From

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useRef } from 'react';
 import './App.css';
-
 function App() {
+  const userNameRef = useRef()
+  const passwordRef = useRef()
+  const emailRef = useRef()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const username = userNameRef.current.value;
+    const password = passwordRef.current.value;
+    const email = emailRef.current.value;
+
+    userNameRef.current.value = '';
+    passwordRef.current.value = '';
+    emailRef.current.value = '';
+
+    const alertMsg = `
+    Your username : ${username}
+    Your password : ${password}
+    Your Email : ${email}
+  `
+
+  alert(alertMsg);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Welcome! Login here</h1>
+        <form onSubmit={handleSubmit}>
+            <div className='input-One'>
+                <label htmlFor="username">User Name:</label>
+                <input ref={userNameRef} type="text" id='username' placeholder='Enter your name' />
+            </div>
+            <br />
+            <div className='input-Two'>
+                <label htmlFor="email">Email:</label>
+                <input ref={emailRef} type="text" id='email' placeholder='Enter your Email' />
+            </div>
+            <br />
+            <div className='input-Three'>
+                <label htmlFor="password">Password:</label>
+                <input ref={passwordRef} type="text" id='password' placeholder='Enter your password' />
+            </div>
+            <br />
+            <button type='submit'>Login</button>
+        </form>
     </div>
   );
 }
---
+++ src/Components/Post.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Post = () => {
+    return (
+        <div>
+            <h1>This is Post Page</h1>
+        </div>
+    );
+};
+
+export default Post;    <h1>This is Post Page</h1>
